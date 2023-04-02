ad620a7d4c1bf67cf5e8465f5135dca5a73bb5f4
dmitry grunt <dmitry.grunt@protonmail.com>
2023-04-02 16:50:46 +0300

    Working mesh merge. To do: fix list index error.

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -6,6 +7,17 @@ using UnityEngine.UIElements;
 
 namespace CityManagerEditor
 {
+
+    struct MapObject
+    {
+        public GameObject instance;
+        public Vector3 position;
+        public MapObject(GameObject gameObject, Vector3 position)
+        {
+            this.instance = gameObject;
+            this.position = position;
+        }
+    }
 
 
     public class MapEditor : EditorWindow
@@ -15,6 +27,7 @@ namespace CityManagerEditor
         private GameObject prefab = null;
         public float gridSize = 1.0f;
         private Vector3 gizmoPosition = Vector3.zero;
+        private List<MapObject> mapObjects = new List<MapObject>();
 
         [SerializeField]
         public VisualTreeAsset m_VisualTreeAsset = default;
@@ -102,36 +115,42 @@ namespace CityManagerEditor
             gridSize = EditorGUILayout.FloatField("Grid Size", gridSize);
             GUILayout.EndVertical();
 
+            GUILayout.BeginVertical();
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Merge Meshes"))
+            {
+                //myScript.MergeMeshes();
+                MergeMeshes();
+            }
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Clear"))
+            {
+                //myScript.MergeMeshes();
+                ClearObjects();
+            }
+
+            GUILayout.EndVertical();
 
         }
 
         [DrawGizmo(GizmoType.NonSelected)]
         private void OnSceneGUI()
         {
-
-            // Draw a gizmo at the gizmoPosition
-            Handles.color = Color.green;
-            Handles.DrawWireCube(gizmoPosition, Vector3.one * gridSize);
+            Renderer prefabRenderer = prefab.GetComponent<MeshRenderer>();
+            Bounds bounds = prefabRenderer.bounds;
 
             // Draw a gizmo at the mouse hit position
             Handles.color = Color.green;
-            Handles.DrawWireCube(gizmoPosition, Vector3.one * gridSize);
-
-            Vector3 nearestGridPoint = GetNearestPointOnGrid(gizmoPosition);
-
-            Debug.Log("Cube at " + nearestGridPoint);
-            Handles.color = Color.green;
-            Handles.DrawWireCube(nearestGridPoint, Vector3.one * gridSize);
-
-
+            Handles.DrawWireCube(gizmoPosition, bounds.size);                       
 
             // Check if the left mouse button is pressed down
             if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
             {
-                // Create a new instance of the selected prefab at the nearest grid point
-                GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-                instance.transform.position = nearestGridPoint;
-                Debug.Log("Spawned " + instance.name + " at " + nearestGridPoint);
+                SpawnObject(prefab, gizmoPosition);
             }
         }
 
@@ -140,32 +159,22 @@ namespace CityManagerEditor
             // Update the gizmo position based on the mouse position
             Event e = Event.current;
             if (e.type == EventType.MouseMove)
-            {
-                gizmoPosition = GetMouseHitPosition(e);
-                SceneView.RepaintAll();
-                Debug.Log("Mouse at " + gizmoPosition);
+            {           
+                var newPos = GetMouseHitPosition(e);
+                gizmoPosition = GetNearestPointOnGrid(newPos);
+                SceneView.RepaintAll();                
             }
         }
 
         private Vector3 GetMouseHitPosition(Event e)
         {
             Vector3 position = Vector3.zero;
-
-            // Calculate the direction from the camera to the mouse cursor
-            Camera currentCamera = SceneView.lastActiveSceneView.camera;
-            Vector3 mousePosition = e.mousePosition;
-            mousePosition.z = currentCamera.nearClipPlane;
-            Vector3 worldPosition = currentCamera.ScreenToWorldPoint(mousePosition);
-            Vector3 direction = (worldPosition - currentCamera.transform.position);
-
-            Debug.DrawLine(currentCamera.transform.position, currentCamera.transform.position + direction * 1000f, Color.yellow);
-
-            // Perform a raycast and check if it hit anything
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(currentCamera.transform.position, direction, out hit, 1000f))
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
             {
                 position = hit.point;
-                Debug.Log("Hit " + position);
             }
 
             return position;
@@ -175,17 +184,143 @@ namespace CityManagerEditor
 
         public Vector3 GetNearestPointOnGrid(Vector3 position)
         {
-            // Calculate the nearest point on the grid based on the current grid size
-            position -= SceneView.lastActiveSceneView.camera.transform.position;
+            // Round the position values to the nearest multiple of gridSize
+            float x = Mathf.Round(position.x / gridSize) * gridSize;
 
-            position = new Vector3(
-                Handles.SnapValue(position.x, gridSize),
-                Handles.SnapValue(position.y, gridSize),
-                Handles.SnapValue(position.z, gridSize)
-            );
+            Renderer prefabRenderer = prefab.GetComponent<MeshRenderer>();
+            Bounds bounds = prefabRenderer.bounds;
 
-            position += SceneView.lastActiveSceneView.camera.transform.position;
-            return position;
+            //float y = Mathf.Round(position.y / gridSize) * gridSize;
+            float y = position.y + bounds.size.y / 2;
+            float z = Mathf.Round(position.z / gridSize) * gridSize;
+
+            // Create a new vector with the rounded values
+            Vector3 newPosition = new Vector3(x, y, z);
+
+            return newPosition;
+        }
+
+
+        private int GetObjectIndexAtPosition(Vector3 position)
+        {
+            for (int i = 0; i < mapObjects.Count; i++)
+            {
+                if (mapObjects[i].position == position)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+
+        private void SpawnObject(GameObject prefab, Vector3 position)
+        {
+            // Check if an object exists at the same position and replace it
+            int existingIndex = GetObjectIndexAtPosition(position);
+            if (existingIndex != -1)
+            {
+                MapObject removeObject = mapObjects[existingIndex];
+                mapObjects.RemoveAt(existingIndex);
+
+                // Destroy the object from the scene
+                if (removeObject.instance != null)
+                {
+                    UnityEngine.Object.DestroyImmediate(removeObject.instance);
+                }
+            }
+
+            // Create a new object and add it to the list
+            //GameObject newObject = new GameObject("New Object");
+            
+
+            // Create a new instance of the selected prefab at the nearest grid point
+            GameObject instance = UnityEngine.Object.Instantiate(prefab, position, Quaternion.identity);
+            MapObject newObject = new MapObject(instance, position);
+            // Set the object's position
+            newObject.instance.transform.position = position;
+            mapObjects.Add(newObject);
+
+            // Make the new object a child of the Holder object
+            if (GameObject.Find("Holder") == null)
+            {
+                GameObject holder = new GameObject("Holder");
+                newObject.instance.transform.parent = holder.transform;
+            }
+            else
+            {
+                newObject.instance.transform.parent = GameObject.Find("Holder").transform;
+            }
+        }
+
+        private void MergeMeshes()
+        {
+            // Get the mesh renderers and filters of all the gameObjects
+            List<MeshFilter> meshFilters = new List<MeshFilter>();
+            List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+            foreach (MapObject item in mapObjects)
+            {
+                MeshFilter meshFilter = item.instance.GetComponent<MeshFilter>();
+                MeshRenderer meshRenderer = item.instance.GetComponent<MeshRenderer>();
+                if (meshFilter != null && meshRenderer != null)
+                {
+                    meshFilters.Add(meshFilter);
+                    meshRenderers.Add(meshRenderer);
+                }
+            }
+
+            // Combine the meshes
+            Mesh combinedMesh = new Mesh();
+            CombineInstance[] combineInstances = new CombineInstance[meshFilters.Count];
+            for (int i = 0; i < meshFilters.Count; i++)
+            {
+                combineInstances[i].mesh = meshFilters[i].sharedMesh;
+                combineInstances[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            }
+            combinedMesh.CombineMeshes(combineInstances, true);
+
+            // Create a new GameObject with the combined mesh
+            GameObject mergedObject = new GameObject("Merged Object");
+            mergedObject.AddComponent<MeshFilter>().sharedMesh = combinedMesh;
+            mergedObject.AddComponent<MeshRenderer>().sharedMaterial = meshRenderers[0].sharedMaterial;
+
+
+            if (GameObject.Find("Holder") == null)
+            {
+                GameObject holder = new GameObject("Holder");
+                mergedObject.transform.parent = holder.transform;
+            }
+
+
+            // Set the position and rotation of the new GameObject to match the first GameObject
+            //mergedObject.transform.position = mapObjects[0].position;
+            mergedObject.transform.rotation = mapObjects[0].instance.transform.rotation;
+
+
+
+            // Delete the original gameObjects
+            for(int i=0; i<mapObjects.Count; i++) 
+            {
+                if (mapObjects[i].instance)
+                {
+                    DestroyImmediate(mapObjects[i].instance);
+                }
+            }
+
+            ClearObjects();
+        }
+
+        private void ClearObjects()
+        {
+            for (int i = 0; i < mapObjects.Count; i++)
+            {
+                //if (mapObjects[i].instance)
+                //{
+                //    DestroyImmediate(mapObjects[i].instance);
+                //}
+                mapObjects.RemoveAt(i);
+            }
         }
 
     }
